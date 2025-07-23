5dc1f3ac6ad1f1070c9230cc8bac87e929a84e2a
Wefrit <lopes.nathanlemes@gmail.com>
2025-07-23 19:14:34 -0300

    Adiciona a pasta Locomotive_engineer

---
+++ Locomotive_engineer/locomotive_engineer.py
@@ -0,0 +1,66 @@
+"""Functions which helps the locomotive engineer to keep track of the train."""
+
+
+def get_list_of_wagons(*wagons):
+    """Return a list of wagons.
+
+    :param: arbitrary number of wagons.
+    :return: list - list of wagons.
+    """
+    return list(wagons)
+
+
+def fix_list_of_wagons(each_wagons_id, missing_wagons):
+    """Fix the list of wagons.
+
+    :param each_wagons_id: list - the list of wagons.
+    :param missing_wagons: list - the list of missing wagons.
+    :return: list - list of wagons.
+    """
+    first, second, third, *rest = each_wagons_id
+    correct_list = [third, *missing_wagons, first, second]
+    return correct_list
+
+
+def add_missing_stops(route, **stops):
+    """Add missing stops to route dict.
+
+    :param route: dict - the dict of routing information.
+    :param: arbitrary number of stops.
+    :return: dict - updated route dictionary.
+    """
+    stop_list = list(stops.values())
+    stop_dict = {"stops": stop_list}
+    combined_route_stop = {**route, **stop_dict}
+
+    return combined_route_stop
+
+
+def extend_route_information(route, more_route_information):
+    """Extend route information with more_route_information.
+
+    :param route: dict - the route information.
+    :param more_route_information: dict -  extra route information.
+    :return: dict - extended route information.
+    """
+    extended_route = {**route, **more_route_information}
+    return extended_route
+    
+
+
+def fix_wagon_depot(wagons_rows):
+    """Fix the list of rows of wagons.
+
+    :param wagons_rows: list[list[tuple]] - the list of rows of wagons.
+    :return: list[list[tuple]] - list of rows of wagons.
+    """
+
+    list_of_row = [list(column) for column in zip(*wagons_rows)]
+    return list_of_row
+
+
+print(fix_wagon_depot([
+                    [(2, "red"), (4, "red"), (8, "red")],
+                    [(5, "blue"), (9, "blue"), (13,"blue")],
+                    [(3, "orange"), (7, "orange"), (11, "orange")],
+                    ]))
