46f1ed966d0e0b00ba45f2b7637e30aa094569cb
Wefrit <lopes.nathanlemes@gmail.com>
2025-08-12 14:10:40 -0300

    Altera a solução do exercicío Resistor_color

--- Resistor_color/resistor_color.py
+++ Resistor_color/resistor_color.py
@@ -1,5 +1,4 @@
-def color_code(color):
-    codes ={"black": 0,
+codes ={"black": 0,
     "brown": 1,
     "red": 2,
     "orange": 3,
@@ -9,21 +8,23 @@ def color_code(color):
     "violet": 7,
     "grey": 8,
     "white": 9}
+def color_code(color):
+    """Returns the numeric value of a resistor color code.
+
+    Args:
+        color (str): The color of the resistor band.
+    Returns:
+        int: The numeric value corresponding to the color. 
+    """
     return codes[color]
 
 
 def colors():
-    colors_list = ["black",
-            "brown",
-            "red",
-            "orange",
-            "yellow",
-            "green",
-            "blue",
-            "violet",
-            "grey",
-            "white"]
-    return colors_list
+    """
+    Returns:
+        list: A list of strings representing the colors of the resistor bands.
+    """
+    return list(codes.keys())
 
 print(color_code("red"))  # Output: 2
 print(colors())  # Output: ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
